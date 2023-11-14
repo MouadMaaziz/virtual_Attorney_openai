7dafa79c105483e5685fd5e75fee8cf5b350a797
MouadMaaziz <morocco8@outlook.fr>
2023-11-14 22:08:08 +0100

    implementing sessions to stope interefering chats

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # app.py
 import os
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request, jsonify, session
 from chat import chatbot, open_file, generate_intake_notes, generate_lawyers_report, generate_scenarios_and_outcomes, prepare_for_form_requirements
 import openai
 from dotenv import load_dotenv
@@ -10,28 +10,35 @@ load_dotenv()
 app = Flask(__name__)
 
 openai.api_key = os.getenv('OPENAI_KEY')
-conversation = list()
+app.secret_key = 'jet39DH-313@'
+
+conversation = []
 conversation.append({'role': 'system', 'content': open_file('system_01_intake.md')})
-user_messages = list()
-all_messages = list()
+all_messages = []
 
 
 @app.route('/')
 def home():
-
+    if 'conversation' in session:
+        session['conversation'] = []
+    if 'all_messages' in session:
+        session['all_messages'] = []
     return render_template('index.html')
     
 
 @app.route('/chat', methods=['GET', 'POST'])
 def chat():
-    
+    if 'conversation' not in session:
+        session['conversation'] = conversation 
+    if 'all_messages' not in session:
+        session['all_messages'] = []
+
     # Intake portion
     while True:
         # get user input
         text = request.args.get('user-input', '')
         if text == 'DONE' or text == '' :
             break
-        user_messages.append(text)
         all_messages.append(f'CLIENT: {text}')
         #store the user's message and the Lawyer's response:
         conversation.append({'role': 'user', 'content': text})
@@ -40,7 +47,7 @@ def chat():
         
         all_messages.append(f'INTAKE: {response_text}') 
         return jsonify({'text': response_text})
-    
+
 
     # Generate intake notes
     notes = generate_intake_notes(all_messages)
@@ -48,17 +55,14 @@ def chat():
     result = request.args.get('results', '')
 
     if result =='report':
-        # Generate lawyer's report
         report = generate_lawyers_report(notes)
         return jsonify({'lawyer_report': report})
     
     elif result =='form':
-        # Prepare for form requirements
         form = prepare_for_form_requirements(notes)
         return jsonify({'form_requirements': form})
     
     elif result =='scenarios':
-        # Generate scenarios and tests
         scenario = generate_scenarios_and_outcomes(notes)
         return jsonify({'scenario_and_outcomes':scenario})
 
--- chat.py
+++ chat.py
@@ -1,4 +1,3 @@
-
 import openai
 from time import time
 
@@ -55,11 +54,6 @@ def chatbot(conversation, model="gpt-4-0613", temperature=0, max_tokens=2000):
         except Exception as oops:
             print(f'\n\nError communicating with OpenAI: "{oops}"')
             exit(5)
-
-# def chat_print(text):
-#     formatted_lines = [textwrap.fill(line, width=120, initial_indent='    ', subsequent_indent='    ') for line in text.split('\n')]
-#     formatted_text = '\n'.join(formatted_lines)
-#     print(f'\n\n\nCHATBOT:\n\n{formatted_text}')
 
 
 def generate_intake_notes(all_messages):
@@ -141,46 +135,5 @@ def generate_scenarios_and_outcomes(notes):
     print(f'\n\nScenario and Outcomes:\n\n{scenario}')
     return scenario
 
-
-
-
-# def main():
-#     openai.api_key = open_file('key_openai.txt').strip()
-
-#     conversation = list()
-#     conversation.append({'role': 'system', 'content': open_file('system_01_intake.md')})
-#     user_messages = list()
-#     all_messages = list()
-#     print('Describe your case to the intake bot. Type DONE when done.')
-
-#     # Intake portion
-
-#     while True:
-#         # get user input
-#         text = input('\n\nCLIENT: ').strip() # text = request.form['user_input']
-#         if text == 'DONE':
-#             break
-#         user_messages.append(text)
-#         all_messages.append(f'CLIENT: {text}')
-#         #store the user's message and the Lawyer's response:
-#         conversation.append({'role': 'user', 'content': text})
-#         response, tokens = chatbot(conversation)
-#         conversation.append({'role': 'assistant', 'content': response})  
-        
-#         all_messages.append(f'INTAKE: {response}') # response = f'INTAKE: {response}'
-#         print(f'\n\nINTAKE: {response}')
-
-
-#      # Generate intake notes
-#     notes = generate_intake_notes(all_messages)
-
-#     # Generate lawyer's report
-#     report = generate_lawyers_report(notes)
-
-#     # Prepare for form requirements
-#     form = prepare_for_form_requirements(notes)
-
-#     # Generate scenarios and tests
-#     scenario = generate_scenarios_and_outcomes(notes)
 
 
